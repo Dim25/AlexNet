e93cb12c236f51855baa7e889e36a825aa678d77
dynmi <dynmi@foxmail.com>
2020-12-09 21:56:26 +0800

    temp

--- test.c
+++ test.c
@@ -1,28 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "alexnet.h"
+#include "src/alexnet.h"
 
 Alexnet alex;
 Feature feat;
 
 int main(void)
 {
-    zero_feats(&feat);
-    for(int p=0; p<BATCH_SIZE; p++)
-    {
-        for(int in_c=0; in_c<IN_CHANNELS; in_c)
-        {
-            for(int i=0; i<FEATURE0_L; i++)
-            {
-                for(int j=0; j<FEATURE0_L; j++)
-                {
-                    feat.input[p][in_c][i][j] = 0.5;
-                }            
-            }
-        }
-        printf("batchID %d \n", p);
-    }
-    
-
+    train(alex, 10);
     return 0;
 }
